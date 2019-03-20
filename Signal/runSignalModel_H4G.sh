d89263a24f2e07b6c7991a5900349ef95bd545fa
wamorkart <wamorkar.t@husky.neu.edu>
2019-03-20 22:37:46 +0100

    script to run parts of signal modeling

---
+++ Signal/runSignalModel_H4G.sh
@@ -0,0 +1,1 @@
+./bin/signalFTest —verbose 1 -i /eos/cms/store/user/twamorka/NTuples_17Feb2019/Test_SignalWS/w_signal_60.root -p Data_13TeV_ -f 4photons -o H4Gamma_a_60_firsttest --datfilename H4Gamma_a_60_firsttest/h4gamma_60.dat
